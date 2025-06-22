57f54fc164b704436487f5aa9a3e7b887125c515
kauanrenner <krennercastro@gmail.com>
2025-06-22 18:37:14 -0300

    erro build

--- src/routes.js
+++ src/routes.js
@@ -2,38 +2,43 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import { TempoReal } from "./Pages/Real";
 import { Programado } from "./Pages/Programado";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons"
+import { View, Text } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
 
 export function Routes() {
     return (
-        <Tab.Navigator swipeEnabled={true} tabBarPosition="bottom" screenOptions={{tabBarStyle:{height:60, backgroundColor:"grey"}, tabBarLabelStyle:{color:"white", fontSize:14}}}>
+        <Tab.Navigator swipeEnabled={true} tabBarPosition="bottom" screenOptions={{tabBarStyle:{height:60, backgroundColor:"grey"}}}>
             <Tab.Screen
                 name="Tempo Real"
                 component={TempoReal}
                 options={{
-                    headerShown:false,
-                    tabBarIcon: ({focused, size, color}) => {
-                        if (focused) {
-                            return <MaterialIcons name="watch-off" size={size+4} color={"white"}/>
-                        }
-
-                        return <MaterialIcons name="watch-off" size={size} color={"white"} />
-                    }
+                    tabBarLabel:({focused}) => (
+                        <View style={{alignItems:"center"}}>
+                            <MaterialIcons 
+                                name="watch-off"
+                                size={focused ? 30 : 26}
+                                color={focused ? "white" : "lightgray"}
+                            />
+                            <Text style={{ color: focused ? 'white' : 'lightgray', fontSize: 14, fontWeight: focused ? "bold" : "normal" }}>Tempo Real</Text>
+                        </View>
+                    ),
                 }}
             />
             <Tab.Screen
                 name="Programado"
                 component={Programado}
                 options={{
-                    headerShown:false,
-                    tabBarIcon: ({focused, size, color}) => {
-                        if (focused) {
-                            return <MaterialIcons name="watch" size={size+4} color={"white"}/>
-                        }
-
-                        return <MaterialIcons name="watch" size={size} color={"white"} />
-                    }
+                    tabBarLabel:({focused}) => (
+                        <View style={{alignItems:"center"}}>
+                            <MaterialIcons 
+                                name="watch"
+                                size={focused ? 30 : 26}
+                                color={focused ? "white" : "lightgray"}
+                            />
+                            <Text style={{ color: focused ? 'white' : 'lightgray', fontSize: 14, fontWeight: focused ? "bold" : "normal" }}>Programado</Text>
+                        </View>
+                    ),
                 }}
             />
         </Tab.Navigator>
